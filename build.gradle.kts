ddd0a898e8997b38ad8001682aabcc6d2d86b967
ZeroClawAIAgent <jemoeder2833@gmail.com>
2026-06-14 03:09:25 +0200

    Match PaperMC test plugin pattern: paperweight 2.0.0-beta.21, Gradle 9.4.1, Java 25, actions/setup-java@v5, checkout@v6, foojay resolver

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
     java
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
 }
 
 group = "com.example"
 version = "1.0"
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
 }
 
 dependencies {
     paperweight.paperDevBundle("26.1.2.build.+")
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
+tasks {
+    compileJava {
+        options.release = 25
+    }
 }
